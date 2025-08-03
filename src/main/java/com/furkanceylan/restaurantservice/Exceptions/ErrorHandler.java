f38df9c1f612c2cb9f5e55ac8c2bd7120b485df8
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-08-04 00:24:53 +0300

    exceptionhandler

--- src/main/java/com/furkanceylan/restaurantservice/Exceptions/ErrorHandler.java
+++ src/main/java/com/furkanceylan/restaurantservice/Exceptions/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.furkanceylan.restaurantservice.Exceptions;
 
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,19 @@ public class ErrorHandler {
               return ResponseEntity.status(404).body(errorResponse);
           }
 
+        @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handler(Exception ex){
+              ErrorResponse response = new ErrorResponse();
+              response.setMessage("An unexpected error occurred");
+              response.setCode("INTERNAL_SERVER_ERROR");
+              return ResponseEntity.status(500).body(response);
+        }
 
+        @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handler(MethodArgumentNotValidException ex){
+              ErrorResponse errorResponse = new ErrorResponse();
+              errorResponse.setMessage("Validation failed : " + ex.getBindingResult().getFieldError().getDefaultMessage());
+            errorResponse.setCode("VALIDATION_ERROR");
+            return ResponseEntity.badRequest().body(errorResponse);
+        }
 }
