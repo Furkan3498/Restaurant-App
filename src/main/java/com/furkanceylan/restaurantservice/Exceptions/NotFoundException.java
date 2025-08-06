8301aef303b32811f4dc83e5a6c494e0e7581866
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-08-06 16:42:28 +0300

    Add restaurant-service and menu-service modules

--- src/main/java/com/furkanceylan/restaurantservice/Exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.furkanceylan.restaurantservice.Exceptions;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/furkanceylan/restaurantservice/repository/RestaurantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.furkanceylan.restaurantservice.repository;
-
-import com.furkanceylan.restaurantservice.entity.RestaurantEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RestaurantRepository extends JpaRepository<RestaurantEntity, Long> {
-}
