4ecf3564a0ca1194398f6584eaa5f9ea3644504d
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-08-06 21:12:33 +0300

    menu-service

--- menu-service/src/main/java/com/example/menuservice/entity/MenuEntity.java
+++ menu-service/src/main/java/com/example/menuservice/entity/MenuEntity.java
@@ -2,8 +2,12 @@ package com.example.menuservice.entity;
 
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Table(name = "menu_items")
 @Entity
@@ -20,4 +24,109 @@ public class MenuEntity {
     @Enumerated(EnumType.STRING)
     private MenuCategory menuCategory;
 
+
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+    @UpdateTimestamp
+    private LocalDateTime updatedTime;
+
+    public MenuEntity() {
+    }
+
+    public MenuEntity(Long id, Long restauranId, String name, String description, BigDecimal price, Boolean isAvailable, MenuCategory menuCategory, LocalDateTime createdTime, LocalDateTime updatedTime) {
+        this.id = id;
+        this.restauranId = restauranId;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.isAvailable = isAvailable;
+        this.menuCategory = menuCategory;
+        this.createdTime = createdTime;
+        this.updatedTime = updatedTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MenuEntity that = (MenuEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRestauranId() {
+        return restauranId;
+    }
+
+    public void setRestauranId(Long restauranId) {
+        this.restauranId = restauranId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Boolean getAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(Boolean available) {
+        isAvailable = available;
+    }
+
+    public MenuCategory getMenuCategory() {
+        return menuCategory;
+    }
+
+    public void setMenuCategory(MenuCategory menuCategory) {
+        this.menuCategory = menuCategory;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public LocalDateTime getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(LocalDateTime updatedTime) {
+        this.updatedTime = updatedTime;
+    }
 }
