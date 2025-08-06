d72a0d9bfb53d3a49d3eb1e2db2870c6355f0c94
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-08-06 21:10:25 +0300

    menu-service

---
+++ menu-service/src/main/java/com/example/menuservice/entity/MenuCategory.java
@@ -0,0 +1,20 @@
+package com.example.menuservice.entity;
+
+public enum MenuCategory {
+
+    DESSER,
+    KEBABS,
+    SALAD,
+    SOUP,
+    PASTA,
+    PIZZA,
+    VEGAN,
+    VEGATARIAN,
+    SEAFOOD,
+    PIDE,
+    MAIN_COURSE,
+    DRINKS,
+    SANDWICH,
+    APPETIZER
+
+}
--- menu-service/src/main/java/com/example/menuservice/entity/MenuEntity.java
+++ menu-service/src/main/java/com/example/menuservice/entity/MenuEntity.java
@@ -1,14 +1,23 @@
 package com.example.menuservice.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
 
 @Table(name = "menu_items")
 @Entity
 
 public class MenuEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private Long restauranId;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Boolean isAvailable;
+    @Enumerated(EnumType.STRING)
+    private MenuCategory menuCategory;
+
 }
