759b2c85696300963bcdea10e8f2ce1f1901e2fd
Furkan <89530770+Furkan3498@users.noreply.github.com>
2025-09-01 19:53:51 +0300

    menu-service

--- menu-service/src/main/java/com/example/menuservice/repository/MenuItemRepository.java
+++ menu-service/src/main/java/com/example/menuservice/repository/MenuItemRepository.java
@@ -3,5 +3,8 @@ package com.example.menuservice.repository;
 import com.example.menuservice.entity.MenuEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MenuItemRepository extends CrudRepository<MenuEntity,Long> {
+    List<MenuEntity> findByRestaurantId(Long restaurantId);
 }
